6b79bb4225cd4384ea6aaded307379d3e4fe7cea
DucTuan <tuanhypc02@gmail.com>
2024-03-29 22:54:14 +0700

    Initial Commit

---
+++ TinderClone/TinderCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TinderCloneApp.swift
+//  TinderClone
+//
+//  Created by macOS on 29/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct TinderCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
